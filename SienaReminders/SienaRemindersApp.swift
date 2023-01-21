26eba801b2ebe3c4e37f9fc8d47361cee0bcb741
dj18pomp <dj18pomp@siena.edu>
2023-01-20 19:31:17 -0500

    Initial Commit

---
+++ SienaReminders/SienaRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SienaRemindersApp.swift
+//  SienaReminders
+//
+//  Created by Dominic Pompilio on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SienaRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
